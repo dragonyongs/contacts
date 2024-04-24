714dbc3bd3bcf42a8697781a586f1d9c9b07b0e6
ADY <ujudge@naver.com>
2024-04-24 18:51:41 +0900

    width  container 제거 및 닫기 버튼 실행 시 인풋 입력값 초기화 로직 추가

--- src/utils/modalUtils.js
+++ src/utils/modalUtils.js
@@ -1,10 +1,33 @@
 function handleOutsideClick() {
   console.log("handleOutsideClick 실행");
+  
+  if (this.modalState === "add" || this.modalState === "search" || this.modalState === "data" || this.modalState === "edit") {
+    // 모달 안의 모든 app-modal-input 요소를 선택한다.
+    const modalInputElements = document.querySelectorAll('#contact-form app-modal-input');
 
-  if (this.modalState === "add" || "search" || "data" || "detail" || "edit") {
+    // 각 app-modal-input 요소의 shadowRoot에 접근하여 input 요소를 찾고 값을 비운다.
+    modalInputElements.forEach(modalInput => {
+      // app-modal-input 요소의 shadowRoot에 접근한다.
+      const shadow = modalInput.shadowRoot;
+
+      // shadowRoot에서 input 요소를 선택한다.
+      const inputElement = shadow.querySelector('input');
+
+      // input 요소의 값을 비운다.
+      inputElement.value = '';
+
+      console.log('Input Reset for', modalInput.getAttribute('data-label'));
+    });
+
+    console.log('addEdit Input Reset!');
+
+    // select 조건 추가 예정
+}
+
+  if (this.modalState === "add" || this.modalState === "search" || this.modalState === "data" || this.modalState === "edit" || this.modalState === "detail") {
     this.modalOpen = false;
     this.modalState = '';
     document.body.classList.remove("active", "overflow-hidden");
   }
-
+  
 }
