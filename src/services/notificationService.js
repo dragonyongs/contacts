17e7f40436feb30cadb9f6584b1a3b466796150d
ADY <ujudge@naver.com>
2024-05-14 14:27:08 +0900

    웹 컴포넌트 버튼 스타일 수정 및 알림 확인 버튼 추가

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -6,20 +6,25 @@ export function notification(message) {
     
     // 알림 모달 요소 생성
     var notificationDiv = document.createElement('div');
-    notificationDiv.className = 'z-50 fixed bottom-20 left-0 w-full h-64 p-8 rounded-tr-3xl rounded-tl-3xl bg-blue-700';
+    notificationDiv.className = 'z-50 fixed bottom-20 left-1/2 -translate-x-1/2 container h-64 p-8 rounded-tr-3xl rounded-tl-3xl bg-blue-700';
 
     // 알림 모달 내부 구성 요소 추가
     notificationDiv.innerHTML = `
         <div class="header mb-8">
             <p class="font-normal text-md text-blue-200">알림</p>
-            <button class="remove-overflow absolute top-6 right-6 bg-white rounded-full shadow-xl w-8 h-8 flex items-center justify-center border focus:bg-gray-100 active:bg-gray-200"
+            <button class="remove-overflow absolute top-6 right-6 bg-transparent text-white rounded-full shadow-xl w-8 h-8 flex items-center justify-center focus:bg-blue-700 active:bg-blue-800"
                 @click="handleOutsideClick">
                 <ion-icon name="close-outline" size="large"></ion-icon>
             </button>
         </div>
-        <div class="content relative h-full">
-            <p class="font-semibold text-2xl text-white">${message}</p>
-            <div class="absolute bottom-9 right-0 w-28 h-28 rounded-full bg-white overflow-hidden flex justify-center items-center">
+        <div class="content relative min-h-32">
+            <div class="w-11/12">
+                <p class="mb-6 font-semibold text-xl text-white">${message}</p>
+                <app-button type="button" role="button" data-iconName="checkmark-outline" data-iconSize="small" class="small remove-overflow w-48 font-semibold text-2xl text-black bg-white rounded-full" @click="handleConfirmClick">
+                    <span slot="label">확인</span>
+                </app-button>
+            </div>
+            <div class="absolute bottom-0 right-0 w-24 h-24 rounded-full bg-white overflow-hidden flex justify-center items-center">
                 <img src="./public/icons/common/icon-animation-bell.gif" class="w-10/12" alt="">
             </div>
         </div>
@@ -28,6 +33,7 @@ export function notification(message) {
     // 알림 모달을 컨테이너에 추가
     notificationContainer.appendChild(notificationDiv);
 }    
+
 // 일정 시간이 지난 후에 모달을 제거 (여기서는 3초 후)
 // setTimeout(function() {
 //     notificationDiv.remove();
